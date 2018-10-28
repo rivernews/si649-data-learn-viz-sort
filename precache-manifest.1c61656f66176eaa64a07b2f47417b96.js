5f14a0a6860c9fae648a028fdc01767f61505454
Shaung Cheng <shaungc@umich.edu>
2018-10-28 16:20:45 -0400

    Updates

---
+++ precache-manifest.1c61656f66176eaa64a07b2f47417b96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/si649-data-learn-viz-sort/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b660740c4b0059c78df9",
+    "url": "/si649-data-learn-viz-sort/static/js/runtime~main.b660740c.js"
+  },
+  {
+    "revision": "9c489c5354bdd75d8faa",
+    "url": "/si649-data-learn-viz-sort/static/js/main.9c489c53.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/si649-data-learn-viz-sort/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "9c489c5354bdd75d8faa",
+    "url": "/si649-data-learn-viz-sort/static/css/main.ca25ab5a.chunk.css"
+  },
+  {
+    "revision": "22015980bd902a0a02211187805d53fd",
+    "url": "/si649-data-learn-viz-sort/index.html"
+  }
+];
